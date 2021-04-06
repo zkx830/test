9b2696611cf316a0fae5228093e7a7eec23892dc
JACK <1649683030@qq.com>
2021-04-06 10:51:02 +0800

    f

---
+++ src/cn/tedu/web/Create_QR.java
@@ -0,0 +1,11 @@
+package cn.tedu.web;
+
+import java.io.File;
+
+public class Create_QR {
+
+	public static void main(String[] args) {
+		
+		qr.CreateQR.make(200, 100, "https://www.baidu.com", "D://22");
+	}
+}
